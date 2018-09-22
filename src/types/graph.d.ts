2117f2800da89563b90d258b99e8935a327138bb
zereight <aldwk68@naver.com>
2018-09-22 06:01:09 +0000

    1.9 Typecheck of gql arguments

--- src/types/graph.d.ts
+++ src/types/graph.d.ts
@@ -1,18 +1,16 @@
-//d.ts파일은 타입스크립트가 type definition ( 타입 정의 )
-//파일이라고 생각하게함.. .d.ts 라는 타입이 따로 있는거임
-//이거 새로고침하면 js로 안바뀔꺼임.
-//근데 난 .ts만 생성됨
+export const typeDefs = ["type Query {\n  sayBye: String!\n  sayHello(name: String!): SayHelloResponse!\n}\n\ntype SayHelloResponse {\n  text: String!\n  error: Boolean!\n}\n"];
+/* tslint:disable */
 
-// export const typeDefs = [
-//         "type Query {\n  sayBye: String!\n  sayHello: Greeting!\n}\n\ntype Greeting {\n  text: String!\n  error: Boolean!\n}\n"
-//     ];
-    
-// export interface Query{
-//     sayBye: string;
-//     sayHello: Greeting;
-// };
+export interface Query {
+  sayBye: string;
+  sayHello: SayHelloResponse;
+}
 
-// export interface Greeting{
-//     text:string;
-//     error:boolean;
-// };
+export interface SayHelloQueryArgs {
+  name: string;
+}
+
+export interface SayHelloResponse {
+  text: string;
+  error: boolean;
+}
