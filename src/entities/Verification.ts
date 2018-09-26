838d99fd241dadbc4e25eac572eabf142fd53147
zereight <aldwk68@naver.com>
2018-09-26 13:41:03 +0000

    1.18 Using Types on the Entities

--- src/entities/Verification.ts
+++ src/entities/Verification.ts
@@ -1,4 +1,6 @@
-import { BaseEntity, Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { BaseEntity, Column,  Entity, PrimaryGeneratedColumn } from "typeorm";
+
+import {verificationTarget} from "../types/types";
 
 @Entity()
 class Verification extends BaseEntity {
@@ -6,8 +8,8 @@ class Verification extends BaseEntity {
     @PrimaryGeneratedColumn()
     id: number;
     
-    @Column({type: "text"})
-    target: string;
+    @Column({type: "text", enum: ["PHONE", "EMAIL"]})
+    target: verificationTarget;
     
     @Column({type: "text"})
     payload: string;
