8e3b9584baf39006033f1d92b991e7a03bde25fc
zereight <aldwk68@naver.com>
2018-09-26 13:59:58 +0000

    1.19 Creating the Verification & Making  Key

--- src/entities/Verification.ts
+++ src/entities/Verification.ts
@@ -1,6 +1,9 @@
-import { BaseEntity, Column,  Entity, PrimaryGeneratedColumn } from "typeorm";
+import { BaseEntity, BeforeInsert, Column,CreateDateColumn,  Entity, PrimaryGeneratedColumn,UpdateDateColumn } from "typeorm";
 
 import {verificationTarget} from "../types/types";
+
+const EMAIL = "EMAIL";
+const PHONE = "PHONE";
 
 @Entity()
 class Verification extends BaseEntity {
@@ -8,7 +11,7 @@ class Verification extends BaseEntity {
     @PrimaryGeneratedColumn()
     id: number;
     
-    @Column({type: "text", enum: ["PHONE", "EMAIL"]})
+    @Column({type: "text", enum: [PHONE, EMAIL]})
     target: verificationTarget;
     
     @Column({type: "text"})
@@ -20,11 +23,21 @@ class Verification extends BaseEntity {
     @Column({type: "boolean"})
     used: boolean;
     
-    @Column({type: "text"})
+    @CreateDateColumn({type: "text"})
     createdAt: string;
     
-    @Column({type: "text"})
+    @UpdateDateColumn({type: "text"})
     updatedAt: string;
+    
+    @BeforeInsert() //타겟 타입에 따라 어떤 키를 생성할지 정함. createKey는 그냥 함수 선언한거라 Entity에 포함안됨.
+    createKey() : void {
+        if (this.target === PHONE){
+            this.key = Math.floor(Math.random() * 100000).toString();
+        }
+        else if (this.target === EMAIL){
+            this.key = Math.random().toString(36).substr(2);
+        }
+    }
 }
 
 export default Verification;
